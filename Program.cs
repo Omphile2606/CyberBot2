d76b71846e4569db1f35d78bd70ea4107b074f3c
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-04-04 10:14:04 +0000

    Greetings and the methods.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using NAudio.Wave;
@@ -10,6 +11,55 @@ namespace CyberBot2
     {
         static void Main(string[] args)
         {
+
+            PlayGreeting();//Playing the voice message that i recorded,
+            DisplayAsciiLogo();//Displaying the ArtLogo that i coded.
+            GreetUser();
+        }
+
+        static void PlayGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer(@"Greeting.wav");
+                player.PlaySync(); // Play synchronously to wait for completion.
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing greeting: {ex.Message}");
+            }
+        }
+
+        static void DisplayAsciiLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@"
+
+   ______      __                _________ __     
+  / ____/_  __/ /_  ___  _____  / ____/ (_) /____ 
+ / /   / / / / __ \/ _ \/ ___/ / __/ / / / __/ _ \
+/ /___/ /_/ / /_/ /  __/ /    / /___/ / / /_/  __/
+\____/\__, /_.___/\___/_/____/_____/_/_/\__/\___/ 
+     /____/            /_____/                    
+
+");
+            Console.ResetColor();
+            Console.WriteLine(new string('-', 40)); // Decorative divider
+        }
+
+        static void GreetUser()
+        {
+            Console.Write("Please enter your name: ");// Asking the user to add their name.
+            string userName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                userName = "User"; // Default name if input is empty
+            }
+
+            Console.WriteLine($"\nHello, {userName}! I'm here to help you stay safe online.");
+            Console.WriteLine("Ask me anything about myself as a chatbox, password safety, phishing, safe browsing, and more.\n");
+            Console.WriteLine(new string('-', 40)); // Decorative divider
         }
     }
 }
